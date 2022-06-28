73751fc5d38427a217ed0ef920b8291cc139dba8
Tyler Sherry <67198813+ArmoredCarnage@users.noreply.github.com>
2022-06-27 21:26:22 -0500

    New assets added to the game along with basic animations/animation blueprint.
    New assets added to the game
    -Animations
    
    Created new assets
    -Animation Blue Print

---
+++ Source/MM_BlackDungeon/TP_ThirdPerson/MortonMorlock.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "MortonMorlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MM_BLACKDUNGEON_API AMortonMorlock : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+};
